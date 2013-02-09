1284875ef0417157073226bc7f0737a91db31512
Adam Keys <adam@therealadam.com>
2013-02-09 16:11:57 -0600

    Nuke jade stuff and make my own views.

--- src/main/scala/stella/StellaServlet.scala
+++ src/main/scala/stella/StellaServlet.scala
@@ -3,15 +3,11 @@ package stella
 import org.scalatra._
 import scalate.ScalateSupport
 
-class StellaServlet extends StellaByStarlightStack {
+class StellaServlet extends StellaByStarlightStack with ScalateSupport {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hey to Stella</a>.
-      </body>
-    </html>
+    contentType = "text/html"
+    ssp("index")
   }
   
 }
